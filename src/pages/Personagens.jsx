2714032e84c07bc78da5d6927cdd3f1377351a16
root <root@DESKTOP-A9GQC9N.localdomain>
2025-10-29 14:40:01 -0300

    Barra de pesquisa de personagens

--- src/pages/Personagens.jsx
+++ src/pages/Personagens.jsx
@@ -3,7 +3,7 @@ import styles from "./Personagens.module.css";
 
 const getFilteredItems = (query, personagens)=> {
   if(!query){
-    return personagens;
+    return [];
   }
   return personagens.filter(personagem => personagem.nome.includes(query))
 };
@@ -11,8 +11,8 @@ const getFilteredItems = (query, personagens)=> {
 export default function Personagens() {
   const [personagens, setPersonagens] = useState([]);
   const [loading, setLoading] = useState(true);
-
   const [query, setQuery] = useState("");
+  const [selectedPersonagem, setSelectedPersonagem] = useState(null);
 
 
 
@@ -37,28 +37,48 @@ export default function Personagens() {
     const filteredItems = getFilteredItems(query, personagens);
 
   return (
-    <>
-    <div className={styles.page_container}>
-      <label>Pesquisar teste</label>
-      <input type="text" value={query} onChange={e => setQuery(e.target.value)} placeholder="Digite o nome do personagem"/>
-      <ul>
-        {filteredItems.map(value => <h1 key={value.id}>{value.nome}</h1>)}
-      </ul>
-    </div>
 
     <div className={styles.page_container}>
       <div className={styles.page_content}>
+        <label htmlFor="search" className="form-label text-white">Pesquisar</label>
+        <input type="text" value={query} onChange={(e) => {
+          setQuery(e.target.value);
+          setSelectedPersonagem(null)//limpa sleção ao digitar
+        }}
+        placeholder="Digite o nome do personagem" className="form-control mb-3"/>
+        
+        <ul className="list-group">
+          {filteredItems.map(value => (
+            <li key={value.id}
+                className="list-group-item bg-dark text-white mb-1"
+                onClick={() => setSelectedPersonagem(value)}
+                style={{cursor: "pointer"}}
+              >
+                <strong>{value.nome}</strong>
+            </li>
+          ))}
+        </ul>
         <h1> Personagens</h1>
         <p>Veja a lista completa de heróis e vilões jogáveis em Marvel Rivals.</p>
       </div>
       <div className={styles.page_content}>
+      </div>
+      {selectedPersonagem && (
+
+      <div className={`${styles.page_content} ${styles.detail_box}`}>
+        
+        <h2>{selectedPersonagem.nome}</h2>
+        <p><strong>Descrição:</strong> {selectedPersonagem.descricao}</p>
+        <p><strong>Dificuldade:</strong> {selectedPersonagem.dificuldade}</p>
+        <p><strong>Afiliação:</strong> {selectedPersonagem.afilicao_principal}</p>
+
         <h1>personagens teste</h1>
-        <ul>{personagens.map((d, index) => (
+        <ul className="list-group">{personagens.map((d, index) => (
           <li key={index}>{d.nome}</li>
           ))}
           </ul>
       </div>
+      )}
     </div>
-    </>
   );
 }
